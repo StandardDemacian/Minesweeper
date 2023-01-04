6d9686866264ad5015ae9ff34a95e51379f96cec
Michael DiFelice <michaeldifelice@michaels-mbp.myfiosgateway.com>
2023-01-04 10:57:23 -0500

    bombs

--- script.js
+++ script.js
@@ -10,23 +10,23 @@ let board = []
 let rows = 8
 let columns = 8 
 
-let minesCount = 5
+let minesCount = 8
 let minesLocation = []
 
 let tilesClicked = 0
 
 let gameOver = false
 
-// startButton.addEventListener("click",gameStart())
+
 window.onload = function() {
     gameStart()
 }
 function setMines() {
-    let minesLeft = minesCount
-    while(minesLeft > 0) {
-        let r = Math.floor(Math.random() * rows)
-        let c = Math.floor(Math.random() * columns)
-        let id = r.toString() + "-" + c.toString()
+     let minesLeft = minesCount
+         while(minesLeft > 0) {
+            let r = Math.floor(Math.random() * rows)
+            let c = Math.floor(Math.random() * columns)
+            let id = r.toString() + "-" + c.toString()
         
         if(!minesLocation.includes(id)) {
             minesLocation.push(id)
@@ -36,6 +36,8 @@ function setMines() {
 }
 
 function gameStart () {
+    setMines() 
+
     for (let r = 0; r < rows; r++){
     let row = []
     for (let c =0; c < columns; c++){
@@ -50,28 +52,37 @@ function gameStart () {
     }
     console.log(board)
 }
-
+console.log(minesLocation)
 function placeFlag () {
  let tile = this
  tile.innerHTML = `<i class="fa-solid fa-flag"></i>`
+ tile.style.backgroundColor = `blue`
 }
 
 function checkTile () {
     let tile = this
-    if(tile.innerHTML == `<i class="fa-solid fa-flag"></i>`) {
-        return
-    }
-    if (minesLocation.includes(tile.id)) {
-        //alert("GAME OVER")
-        gameOver = true
+    if (tile.innerHTML= `<i class="fa-solid fa-flag"></i>`){
+         tile.innerHTML = ""
+         tile.style.backgroundColor = "lightgray"
+        }
+    if (minesLocation.includes(tile.id)){
+        alert("KABOOM")
         revealMines()
         return
     }
-
-    let coordinates = tile.id.split("-")
-    let r = parseInt(coordinates[0])
-    let c = parseInt(coordinates[1])
-    checkMine(r,c)
-
+}
+//Reveal Mines Function
+function revealMines() {
+    for(let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = board[r][c]
+            if(minesLocation.includes(tile.id)){
+                tile.innerHTML = `<i class="fa-solid fa-land-mine-on"></i>`
+                 tile.style.backgroundColor = "red"
+            }
+        }
+    }
 }
 
+
+ //Make check Mine function
